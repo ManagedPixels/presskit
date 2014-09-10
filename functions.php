eb15f5ff5d9ffaa2624dccb01982bb478ec9fb16
shawnsandy <shawnsandy04@gmail.com>
2014-09-10 15:05:58 -0400

    Testing plugins
    Signed-off-by: shawnsandy <shawnsandy04@gmail.com>

--- functions.php
+++ functions.php
@@ -183,4 +183,8 @@ add_action('wp_enqueue_scripts', 'bj_scripts');
 
 //$foo->print_it();
 
-$am = new MpLoader\Admin\Menu();
+//$amenu = MpLoader\Admin\Menu::factory();
+$press_adminbar = MpLoader\Admin\PostMenus::factory()->create_nodes();
+
+$press_sample = MpLoader\Post\CustomTypes::factory('inde_shop')->register_post_type('My Shop');
+
